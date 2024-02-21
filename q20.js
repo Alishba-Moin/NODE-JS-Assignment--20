29359350685ed8ece38575645184a69574c4b823
Alishba-Moin <159716625+Alishba-Moin@users.noreply.github.com>
2024-02-21 06:11:23 -0800

    Add files via upload

---
+++ q20.js
@@ -0,0 +1,6 @@
+// 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+var countries = ["UK", "USA", "China", "India", "Russia", "Brazil", "Germany", "France", "Japan", "Canada"];
+console.log("LIST OF COUNTRIES:");
+countries.forEach(function (country) {
+    console.log(country);
+});
